41df6dcdb09a1b710876aad76371a989c978086d
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2024-04-04 17:31:00 +0200

    usdk: remove return values from mem functions

--- usdk/include/usdk/util/mem.h
+++ usdk/include/usdk/util/mem.h
@@ -5,20 +5,18 @@
 
 namespace usdk {
 
-[[gnu::always_inline]] inline constexpr void * strcpy(char *dest, const char *src) {
+[[gnu::always_inline]] inline constexpr void strcpy(char *dest, const char *src) {
     char* dest8 = dest;
     const char* src8 = src;
 
     while (*src8) *dest8++ = *src8++;
-
-    return dest;
 }
 
-[[gnu::noinline]] inline constexpr void * strcpy_noinline(char *dest, const char *src) {
-    return strcpy(dest, src);
+[[gnu::noinline]] inline constexpr void strcpy_noinline(char *dest, const char *src) {
+    strcpy(dest, src);
 }
 
-[[gnu::always_inline]] inline void * memmove(void *dest, const void *src, size_t length) {
+[[gnu::always_inline]] inline void memmove(void *dest, const void *src, size_t length) {
     uint8_t* dest8 = (uint8_t*) dest;
     const uint8_t* src8 = (const uint8_t*) src;
 
@@ -29,12 +27,10 @@ namespace usdk {
         dest8 += length - 1;
         while (length--) *dest8-- = *src8--;
     }
-
-    return dest;
 }
 
-[[gnu::noinline]] inline void * memmove_noinline(void *dest, const void *src, size_t length) {
-    return memmove(dest, src, length);
+[[gnu::noinline]] inline void memmove_noinline(void *dest, const void *src, size_t length) {
+    memmove(dest, src, length);
 }
 
 }
